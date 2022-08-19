442b0b52c988ea06f44bff8b863668d492544c10
Jeong Sup Park <89337721+MozziPotato@users.noreply.github.com>
2022-08-19 17:24:08 +0900

    Time: 612 ms (90.09%), Space: 13.9 MB (59.58%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -7,19 +7,19 @@ class Solution:
         
         # current longest length를 참고하여 loop(N^3) -> Accepted(Runtime 9117ms)
         
-        longestPalindrome = ""
-        length = 0
+        #longestPalindrome = ""
+        #length = 0
         
-        for i in range(len(s)):
-            curr = s[i:i+length]
-            for j in range(i+length,len(s)):
-                curr += s[j]
-                if self.isPalindrome(curr):
-                    if len(curr) > len(longestPalindrome):
-                        longestPalindrome = curr
-                        length = len(longestPalindrome)
-        
-        return longestPalindrome
+        #for i in range(len(s)):
+        #    curr = s[i:i+length]
+        #    for j in range(i+length,len(s)):
+        #        curr += s[j]
+        #        if self.isPalindrome(curr):
+        #            if len(curr) > len(longestPalindrome):
+        #                longestPalindrome = curr
+        #                length = len(longestPalindrome)
+        #
+        #return longestPalindrome
         
         ############################################################################
     
@@ -42,9 +42,31 @@ class Solution:
         #
         #return longestPalindrome
                 
-    def isPalindrome(self, s: str) -> bool:
+    #def isPalindrome(self, s: str) -> bool:
         
-        if s == s[::-1]:
-            return True
-        else:
-            return False
+    #    if s == s[::-1]:
+    #        return True
+    #    else:
+    #        return False
+    
+        n = len(s)
+        res = ""
+        maxl = 0
+
+        for i in range(0,n):
+            l, r = i, i
+            while l >= 0 and r < n and s[l] == s[r]:
+                if r-l+1 > maxl:
+                    res = s[l:r+1]
+                    maxl = r-l+1
+                l -= 1
+                r += 1
+
+            l, r = i, i+1
+            while l >= 0 and r < n and s[l] == s[r]:
+                if r-l+1 > maxl:
+                    res = s[l:r+1]
+                    maxl = r-l+1
+                l -= 1
+                r += 1
+        return res
