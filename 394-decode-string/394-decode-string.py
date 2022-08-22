342a4cb1a08dba7e9604a775f28ada2326662ceb
Jeong Sup Park <89337721+MozziPotato@users.noreply.github.com>
2022-08-22 17:42:59 +0900

    Time: 34 ms (86.41%), Space: 14 MB (20.40%) - LeetHub

---
+++ 394-decode-string/394-decode-string.py
@@ -0,0 +1,60 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        # ASCII numbers: 48 ~ 57
+        # ASCII alphabets: 97 ~ 122
+        
+        stack = []
+        
+        bracket_count = 0
+        bracket = ""
+        output = ""
+        
+        for ch in s:
+            
+            if ch == "[":
+                if bracket_count == 0:
+                    bracket_count += 1
+                else:
+                    bracket_count += 1
+                    bracket += ch
+            
+            elif ch == "]":
+                if bracket_count > 1 :
+                    bracket_count -= 1
+                    bracket += ch
+                else:
+                    bracket_count -= 1
+                    stack.append(bracket)
+                    bracket = ""
+            
+            elif bracket_count > 0:
+                bracket += ch
+            
+            else:
+                stack.append(ch)
+                
+        number = ""
+        times = False
+        
+        for element in stack:
+            
+            if len(element) > 1:
+                tmp = self.decodeString(element)
+                number = int(number)
+                output += number * tmp
+                number = ""
+                
+            elif 48 <= ord(element) <= 57:
+                number += element
+                times = True
+              
+            else:
+                if number != "":
+                    number = int(number)
+                    output += number * element
+                    number = ""
+                else:
+                    output += element
+                
+        return output
