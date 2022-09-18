152b9356153234b74a43d5f40bec199ac6ae0923
Jeong Sup Park <89337721+MozziPotato@users.noreply.github.com>
2022-09-18 15:56:05 +0900

    Time: 859 ms (13.74%), Space: 10.2 MB (74.69%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> output;
+        for(int i = 0; i < nums.size()-1; i++){
+            for(int j = i+1; j < nums.size(); j++){
+                if (nums[i] + nums[j] == target){
+                    output = {i, j};
+                }
+            }
+        }
+        return output;
+    }
+};
