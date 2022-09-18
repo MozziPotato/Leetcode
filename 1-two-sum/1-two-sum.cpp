16d436104869300f819d5e2b2adad1eba16873be
Jeong Sup Park <89337721+MozziPotato@users.noreply.github.com>
2022-09-18 16:04:27 +0900

    Time: 29 ms (51.36%), Space: 10.9 MB (33.18%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> output;
-        bool found = false;
-        for(int i = 0; i < nums.size()-1; i++){
-            for(int j = i+1; j < nums.size(); j++){
-                if (nums[i] + nums[j] == target){
-                    output = {i, j};
-                    found = true;
-                    break;
-                }
+        unordered_map<int, int> hashtable;
+        for(int i = 0; i < nums.size(); i++){
+            auto it = hashtable.find(target - nums[i]);
+            if(it != hashtable.end()){
+                return {i, it->second};
             }
-            if(found){
-                break;
-            }
+            hashtable[nums[i]] = i;
         }
-        return output;
+        return {};
     }
 };
